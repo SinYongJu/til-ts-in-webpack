b79fc201c6fe8357c19212aa97c7a5e7fbfa07b9
lukas.sin <lukas.sin@kakaocorp.com>
2020-06-15 19:15:37 +0900

    init html and make util

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,69 @@
-interface Ainterface {
-  alphabet : string,
-  id : number
+import { setStyles,querySelect, Style } from "./util/index";
+// app context
+const body = querySelect("body")
+const appCtx = querySelect("#app")
+const deleteButton = querySelect("#btn_delete")
+const startButton = querySelect("#btn_start")
+const initButton = querySelect("#btn_init")
+
+const APP_STYLE:Style = {
+  width: '500px',
+  height: '500px',
+  backgroundColor: '#fff'
+}
+const BODY_STYLE:Style = {
+  backgroundColor: '#eee',
+  margin:'0',
+  padding:'0'
 }
 
-function A (a : Ainterface):void{
-  console.log(`이 알파벳은 ${a.alphabet} 번째 ${a.alphabet} 입니다`)
+const RECT_STYLE:Style = {
+  backgroundColor: '#red',
+  margin:'0',
+  padding:'0',
+  width:'50px',
+  height:'50px'
 }
 
 
-A({alphabet: 'a', id: 1})
+const createRect = (x:number,y:number) => {
+  let div:HTMLDivElement = document.createElement('div')
+  setStyles(div,RECT_STYLE)
+  x&&(div.style.top = x +"px")
+  y&&(div.style.left = y +"px")
+}
+
+
+
+
+
+function move(time:number){
+  return () => {
+    console.log(time)
+    if(moveId === null) moveId = setInterval(()=>{
+      console.log('여기')
+    },time)
+  }
+}
+
+let moveId:NodeJS.Timeout=null;
+
+const deleteInterval=(event:Event) => {
+  if(moveId !== null) {
+    clearInterval(moveId)
+    moveId = null
+  }  
+
+}
+
+function init():void{
+  setStyles(body, BODY_STYLE)
+  setStyles(appCtx, APP_STYLE)
+  startButton.addEventListener('click',move(1000))
+  deleteButton.addEventListener('click',deleteInterval)
+  // initButton.addEventListener('click',deleteInterval)
+
+}
+
+init()
+
---
+++ src/util/index.ts
@@ -0,0 +1,15 @@
+export interface Style {
+    [key: string]: string
+}
+
+export function setStyles (el : HTMLElement, style : Style){
+    Object.keys(style).forEach((key : string)=>{
+        let elStyle = el.style as CSSStyleDeclaration
+        (<any>elStyle)[key] = style[key];
+    })
+}
+
+
+export function querySelect (selector:string) : HTMLElement {
+    return document.querySelector<HTMLElement>(selector)
+}
